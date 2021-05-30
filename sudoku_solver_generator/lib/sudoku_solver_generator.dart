186dbc1ee5f185787d431b52f170595d014d3bd5
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-05-30 17:28:04 +0530

    Removed unnecessary comments

--- sudoku_solver_generator/lib/sudoku_solver_generator.dart
+++ sudoku_solver_generator/lib/sudoku_solver_generator.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
 ///
-/// More dartdocs go here.
 library sudoku_solver_generator;
 
 export 'src/sudoku_solver_generator_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.

77b5165369f09e48cc17a9659330a195ad8fdc2c
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-05-30 17:31:17 +0530

    Added example for SudokuSolver class

--- sudoku_solver_generator/example/sudoku_solver_generator_example.dart
+++ sudoku_solver_generator/example/sudoku_solver_generator_example.dart
@@ -1,6 +1,20 @@
 import 'package:sudoku_solver_generator/sudoku_solver_generator.dart';
 
 void main() {
-  /*var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');*/
+  // Solve Example
+  var grid = <List<int>>[
+    [7, 0, 2, 0, 3, 0, 0, 6, 0],
+    [0, 0, 0, 0, 0, 0, 0, 8, 0],
+    [0, 4, 0, 0, 9, 0, 0, 0, 0],
+    [0, 6, 7, 0, 1, 9, 0, 0, 5],
+    [0, 5, 0, 2, 0, 3, 0, 0, 4],
+    [0, 0, 0, 0, 5, 0, 7, 0, 0],
+    [0, 3, 0, 7, 0, 0, 0, 5, 0],
+    [0, 7, 0, 0, 0, 5, 0, 0, 0],
+    [0, 0, 5, 0, 4, 0, 0, 7, 6]
+  ];
+  var sudokuSolver = SudokuSolver(grid);
+  for (var row in sudokuSolver.solution) {
+    print(row);
+  }
 }

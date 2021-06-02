57f96b43dee28f4cd92bee5842b2418652b11f2e
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-06-03 00:36:38 +0530

    Added function to check if Sudoku configuration is valid

--- sudoku_solver_generator/lib/src/sudoku_utilities_base.dart
+++ sudoku_solver_generator/lib/src/sudoku_utilities_base.dart
@@ -49,4 +49,61 @@ class SudokuUtilities {
     }
     return sudoku2D;
   }
+
+  static bool isValidConfiguration(List<List<int>> sudoku) {
+    bool notInRow(List<List<int>> sudoku, int rowNumber) {
+      var numberSet = <int>{};
+      for (var i = 0; i < 9; i++) {
+        if (numberSet.contains(sudoku[rowNumber][i])) {
+          return false;
+        } else if (sudoku[rowNumber][i] != 0) {
+          numberSet.add(sudoku[rowNumber][i]);
+        }
+      }
+      return true;
+    }
+
+    bool notInColumn(List<List<int>> sudoku, int columnNumber) {
+      var numberSet = <int>{};
+      for (var i = 0; i < 9; i++) {
+        if (numberSet.contains(sudoku[i][columnNumber])) {
+          return false;
+        } else if (sudoku[i][columnNumber] != 0) {
+          numberSet.add(sudoku[i][columnNumber]);
+        }
+      }
+      return true;
+    }
+
+    bool notInBox(List<List<int>> sudoku, int startRow, int startColumn) {
+      var numberSet = <int>{};
+      for (var row = 0; row < 3; row++) {
+        for (var column = 0; column < 3; column++) {
+          if (numberSet
+              .contains(sudoku[row + startRow][column + startColumn])) {
+            return false;
+          } else if (sudoku[row + startRow][column + startColumn] != 0) {
+            numberSet.add(sudoku[row + startRow][column + startColumn]);
+          }
+        }
+      }
+      return true;
+    }
+
+    bool isValid(List<List<int>> sudoku, int rowNumber, int columnNumber) {
+      return notInRow(sudoku, rowNumber) &&
+          notInColumn(sudoku, columnNumber) &&
+          notInBox(sudoku, rowNumber - rowNumber % 3,
+              columnNumber - columnNumber % 3);
+    }
+
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        if (!isValid(sudoku, i, j)) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }

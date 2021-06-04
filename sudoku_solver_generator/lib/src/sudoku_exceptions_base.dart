34d31f2128e24f5b40011806676d3de682b16213
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-06-04 20:35:07 +0530

    Documented InvalidSudokuConfigurationException class

--- sudoku_solver_generator/lib/src/sudoku_exceptions_base.dart
+++ sudoku_solver_generator/lib/src/sudoku_exceptions_base.dart
@@ -1,4 +1,15 @@
+/// Thrown when the configuration of the Sudoku is not valid.
+/// Configuration of the Sudoku follows the rules of Sudoku.
 ///
+/// Conditions:
+/// * [List] of 9 [List]s of 9 [int]s
+/// * Every row must have the numbers 1-9 without repetition
+/// * Every column must have the numbers 1-9 without repetition
+/// * Every 3x3 box must have the number 1-9 without repetition
+/// * Only positions of non-zero and non-null values are considered
+/// * Solvability of the puzzle is not considered
+///
+/// [SudokuUtilities.isValidConfiguration()] can be used to check for validity to prevent this Exception.
 class InvalidSudokuConfigurationException implements Exception {
   String errorMessage() =>
       'The configuration for the Sudoku puzzle is invalid.';

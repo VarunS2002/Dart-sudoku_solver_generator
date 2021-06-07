1525d7a504f506b3657fee79b19bb31184fef42c
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-06-07 15:15:36 +0530

    Grouped tests by class

--- sudoku_solver_generator/test/sudoku_solver_generator_test.dart
+++ sudoku_solver_generator/test/sudoku_solver_generator_test.dart
@@ -2,226 +2,235 @@ import 'package:sudoku_solver_generator/sudoku_solver_generator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('SudokuGenerator unsolved tests', () {
-    test('Default - 27', () {
-      var generator = SudokuGenerator();
-      expect(SudokuUtilities.isValidConfiguration(generator.newSudoku), isTrue);
+  group('SudokuGenerator Tests', () {
+    group('SudokuGenerator unsolved tests', () {
+      test('Default - 27', () {
+        var generator = SudokuGenerator();
+        expect(
+            SudokuUtilities.isValidConfiguration(generator.newSudoku), isTrue);
+      });
+
+      test('Beginner - 18', () {
+        var generator = SudokuGenerator(18);
+        expect(
+            SudokuUtilities.isValidConfiguration(generator.newSudoku), isTrue);
+      });
+
+      test('Easy - 27', () {
+        var generator =
+            SudokuGenerator(27); // ignore: avoid_redundant_argument_values
+        expect(
+            SudokuUtilities.isValidConfiguration(generator.newSudoku), isTrue);
+      });
+
+      test('Medium - 36', () {
+        var generator = SudokuGenerator(36);
+        expect(
+            SudokuUtilities.isValidConfiguration(generator.newSudoku), isTrue);
+      });
+
+      test('Hard - 54', () {
+        var generator = SudokuGenerator(54);
+        expect(
+            SudokuUtilities.isValidConfiguration(generator.newSudoku), isTrue);
+      });
     });
 
-    test('Beginner - 18', () {
-      var generator = SudokuGenerator(18);
-      expect(SudokuUtilities.isValidConfiguration(generator.newSudoku), isTrue);
+    group('SudokuGenerator solved tests', () {
+      test('Default', () {
+        var generator = SudokuGenerator();
+        expect(SudokuUtilities.isSolved(generator.newSudokuSolved), isTrue);
+      });
+
+      test('Beginner - 18', () {
+        var generator = SudokuGenerator(18);
+        expect(SudokuUtilities.isValidConfiguration(generator.newSudokuSolved),
+            isTrue);
+      });
+
+      test('Easy - 27', () {
+        var generator =
+            SudokuGenerator(27); // ignore: avoid_redundant_argument_values
+        expect(SudokuUtilities.isValidConfiguration(generator.newSudokuSolved),
+            isTrue);
+      });
+
+      test('Medium - 36', () {
+        var generator = SudokuGenerator(36);
+        expect(SudokuUtilities.isValidConfiguration(generator.newSudokuSolved),
+            isTrue);
+      });
+
+      test('Hard - 54', () {
+        var generator = SudokuGenerator(54);
+        expect(SudokuUtilities.isValidConfiguration(generator.newSudokuSolved),
+            isTrue);
+      });
     });
 
-    test('Easy - 27', () {
-      var generator =
-          SudokuGenerator(27); // ignore: avoid_redundant_argument_values
-      expect(SudokuUtilities.isValidConfiguration(generator.newSudoku), isTrue);
-    });
+    group('SudokuGenerator exception tests', () {
+      test('<1 emptySquares', () {
+        expect(() => SudokuGenerator(0), throwsException);
+      });
 
-    test('Medium - 36', () {
-      var generator = SudokuGenerator(36);
-      expect(SudokuUtilities.isValidConfiguration(generator.newSudoku), isTrue);
-    });
-
-    test('Hard - 54', () {
-      var generator = SudokuGenerator(54);
-      expect(SudokuUtilities.isValidConfiguration(generator.newSudoku), isTrue);
-    });
-  });
-
-  group('SudokuGenerator solved tests', () {
-    test('Default', () {
-      var generator = SudokuGenerator();
-      expect(SudokuUtilities.isSolved(generator.newSudokuSolved), isTrue);
-    });
-
-    test('Beginner - 18', () {
-      var generator = SudokuGenerator(18);
-      expect(SudokuUtilities.isValidConfiguration(generator.newSudokuSolved),
-          isTrue);
-    });
-
-    test('Easy - 27', () {
-      var generator =
-          SudokuGenerator(27); // ignore: avoid_redundant_argument_values
-      expect(SudokuUtilities.isValidConfiguration(generator.newSudokuSolved),
-          isTrue);
-    });
-
-    test('Medium - 36', () {
-      var generator = SudokuGenerator(36);
-      expect(SudokuUtilities.isValidConfiguration(generator.newSudokuSolved),
-          isTrue);
-    });
-
-    test('Hard - 54', () {
-      var generator = SudokuGenerator(54);
-      expect(SudokuUtilities.isValidConfiguration(generator.newSudokuSolved),
-          isTrue);
-    });
-  });
-
-  group('SudokuGenerator exception tests', () {
-    test('<1 emptySquares', () {
-      expect(() => SudokuGenerator(0), throwsException);
-    });
-
-    test('>81 emptySquares', () {
-      expect(() => SudokuGenerator(82), throwsException);
-    });
-  });
-
-  group('SudokuSolver tests', () {
-    test('With null', () {
-      var sudoku = <List<int?>>[
-        [2, 1, 7, 4, 3, 5, 9, null, 8],
-        [5, 8, 9, 0, 6, 2, 4, 3, null],
-        [3, 4, 6, 0, 7, 9, 2, 5, 1],
-        [1, 6, 2, 3, 0, 0, 5, 4, 0],
-        [9, 0, 5, 2, 0, 4, 7, 8, 6],
-        [4, 7, 8, 5, 9, 6, 1, 0, 0],
-        [8, 0, 3, 0, 4, 1, 6, 7, 2],
-        [0, 9, 4, 7, 2, 3, 8, 1, 5],
-        [7, 2, 1, 0, 5, 0, 0, 0, 4],
-      ];
-      expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
-    });
-
-    test('Null safe', () {
-      var sudoku = <List<int?>>[
-        [2, 1, 7, 4, 3, 5, 9, 0, 8],
-        [5, 8, 9, 0, 6, 2, 4, 3, 0],
-        [3, 4, 6, 0, 7, 9, 2, 5, 1],
-        [1, 6, 2, 3, 0, 0, 5, 4, 0],
-        [9, 0, 5, 2, 0, 4, 7, 8, 6],
-        [4, 7, 8, 5, 9, 6, 1, 0, 0],
-        [8, 0, 3, 0, 4, 1, 6, 7, 2],
-        [0, 9, 4, 7, 2, 3, 8, 1, 5],
-        [7, 2, 1, 0, 5, 0, 0, 0, 4],
-      ];
-      expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
-    });
-
-    test('Already Solved', () {
-      var sudoku = <List<int?>>[
-        [2, 1, 7, 4, 3, 5, 9, 6, 8],
-        [5, 8, 9, 1, 6, 2, 4, 3, 7],
-        [3, 4, 6, 8, 7, 9, 2, 5, 1],
-        [1, 6, 2, 3, 8, 7, 5, 4, 9],
-        [9, 3, 5, 2, 1, 4, 7, 8, 6],
-        [4, 7, 8, 5, 9, 6, 1, 2, 3],
-        [8, 5, 3, 9, 4, 1, 6, 7, 2],
-        [6, 9, 4, 7, 2, 3, 8, 1, 5],
-        [7, 2, 1, 6, 5, 8, 3, 9, 4],
-      ];
-      expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
-    });
-
-    test('Easy - 27', () {
-      var sudoku = <List<int?>>[
-        [2, 7, 0, 1, 0, 3, 8, 0, 5],
-        [0, 0, 9, 2, 0, 8, 3, 0, 7],
-        [3, 5, 0, 7, 0, 6, 0, 1, 4],
-        [4, 0, 7, 8, 0, 5, 9, 3, 0],
-        [5, 6, 0, 4, 3, 0, 7, 8, 1],
-        [9, 0, 3, 0, 0, 1, 5, 0, 0],
-        [7, 3, 4, 0, 6, 2, 0, 5, 0],
-        [8, 2, 5, 3, 0, 4, 6, 7, 0],
-        [6, 9, 1, 0, 0, 7, 4, 2, 3],
-      ];
-      expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
-    });
-
-    test('Hard - 54', () {
-      var sudoku = <List<int?>>[
-        [0, 6, 0, 1, 0, 0, 0, 0, 0],
-        [0, 8, 0, 0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 9, 5, 0, 4, 6],
-        [0, 0, 0, 0, 6, 1, 9, 7, 5],
-        [0, 0, 6, 0, 5, 0, 4, 3, 0],
-        [0, 0, 9, 7, 0, 0, 0, 0, 0],
-        [0, 0, 0, 4, 1, 0, 5, 0, 0],
-        [5, 0, 2, 0, 0, 3, 0, 0, 0],
-        [0, 0, 0, 5, 0, 2, 0, 0, 0],
-      ];
-      expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
-    });
-
-    test('Multiple solutions - 80', () {
-      var sudoku = <List<int?>>[
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 9, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      ];
-      expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
+      test('>81 emptySquares', () {
+        expect(() => SudokuGenerator(82), throwsException);
+      });
     });
   });
 
-  group('SudokuSolver exception tests', () {
-    test('Invalid Sudoku - Repetitive numbers', () {
-      var sudoku = <List<int?>>[
-        [2, 1, 7, 4, 3, 5, 9, 2, 8],
-        [5, 8, 9, 0, 6, 2, 4, 3, 0],
-        [3, 4, 6, 0, 7, 9, 2, 5, 1],
-        [1, 6, 2, 3, 0, 0, 5, 4, 0],
-        [9, 0, 5, 2, 0, 4, 7, 8, 6],
-        [4, 7, 8, 5, 9, 6, 1, 0, 0],
-        [8, 0, 3, 0, 4, 1, 6, 7, 2],
-        [0, 9, 4, 7, 2, 3, 8, 1, 5],
-        [7, 2, 1, 0, 5, 0, 0, 0, 4],
-      ];
-      expect(() => SudokuSolver.solve(sudoku), throwsException);
+  group('SudokuSolver Tests', () {
+    group('SudokuSolver solving tests', () {
+      test('With null', () {
+        var sudoku = <List<int?>>[
+          [2, 1, 7, 4, 3, 5, 9, null, 8],
+          [5, 8, 9, 0, 6, 2, 4, 3, null],
+          [3, 4, 6, 0, 7, 9, 2, 5, 1],
+          [1, 6, 2, 3, 0, 0, 5, 4, 0],
+          [9, 0, 5, 2, 0, 4, 7, 8, 6],
+          [4, 7, 8, 5, 9, 6, 1, 0, 0],
+          [8, 0, 3, 0, 4, 1, 6, 7, 2],
+          [0, 9, 4, 7, 2, 3, 8, 1, 5],
+          [7, 2, 1, 0, 5, 0, 0, 0, 4],
+        ];
+        expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
+      });
+
+      test('Null safe', () {
+        var sudoku = <List<int?>>[
+          [2, 1, 7, 4, 3, 5, 9, 0, 8],
+          [5, 8, 9, 0, 6, 2, 4, 3, 0],
+          [3, 4, 6, 0, 7, 9, 2, 5, 1],
+          [1, 6, 2, 3, 0, 0, 5, 4, 0],
+          [9, 0, 5, 2, 0, 4, 7, 8, 6],
+          [4, 7, 8, 5, 9, 6, 1, 0, 0],
+          [8, 0, 3, 0, 4, 1, 6, 7, 2],
+          [0, 9, 4, 7, 2, 3, 8, 1, 5],
+          [7, 2, 1, 0, 5, 0, 0, 0, 4],
+        ];
+        expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
+      });
+
+      test('Already Solved', () {
+        var sudoku = <List<int?>>[
+          [2, 1, 7, 4, 3, 5, 9, 6, 8],
+          [5, 8, 9, 1, 6, 2, 4, 3, 7],
+          [3, 4, 6, 8, 7, 9, 2, 5, 1],
+          [1, 6, 2, 3, 8, 7, 5, 4, 9],
+          [9, 3, 5, 2, 1, 4, 7, 8, 6],
+          [4, 7, 8, 5, 9, 6, 1, 2, 3],
+          [8, 5, 3, 9, 4, 1, 6, 7, 2],
+          [6, 9, 4, 7, 2, 3, 8, 1, 5],
+          [7, 2, 1, 6, 5, 8, 3, 9, 4],
+        ];
+        expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
+      });
+
+      test('Easy - 27', () {
+        var sudoku = <List<int?>>[
+          [2, 7, 0, 1, 0, 3, 8, 0, 5],
+          [0, 0, 9, 2, 0, 8, 3, 0, 7],
+          [3, 5, 0, 7, 0, 6, 0, 1, 4],
+          [4, 0, 7, 8, 0, 5, 9, 3, 0],
+          [5, 6, 0, 4, 3, 0, 7, 8, 1],
+          [9, 0, 3, 0, 0, 1, 5, 0, 0],
+          [7, 3, 4, 0, 6, 2, 0, 5, 0],
+          [8, 2, 5, 3, 0, 4, 6, 7, 0],
+          [6, 9, 1, 0, 0, 7, 4, 2, 3],
+        ];
+        expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
+      });
+
+      test('Hard - 54', () {
+        var sudoku = <List<int?>>[
+          [0, 6, 0, 1, 0, 0, 0, 0, 0],
+          [0, 8, 0, 0, 0, 0, 0, 0, 1],
+          [0, 0, 0, 0, 9, 5, 0, 4, 6],
+          [0, 0, 0, 0, 6, 1, 9, 7, 5],
+          [0, 0, 6, 0, 5, 0, 4, 3, 0],
+          [0, 0, 9, 7, 0, 0, 0, 0, 0],
+          [0, 0, 0, 4, 1, 0, 5, 0, 0],
+          [5, 0, 2, 0, 0, 3, 0, 0, 0],
+          [0, 0, 0, 5, 0, 2, 0, 0, 0],
+        ];
+        expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
+      });
+
+      test('Multiple solutions - 80', () {
+        var sudoku = <List<int?>>[
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 9, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+        expect(SudokuUtilities.isSolved(SudokuSolver.solve(sudoku)), isTrue);
+      });
     });
 
-    test('Invalid Sudoku - Wrong numbers', () {
-      var sudoku = <List<int?>>[
-        [2, 1, 7, 4, 3, 5, 9, 29, 8],
-        [5, 8, 9, 0, 6, 2, 4, 3, 0],
-        [3, 4, 6, 0, 7, 9, 2, 5, 1],
-        [1, 6, 2, 3, 0, 0, 5, 4, 0],
-        [9, 0, 5, 2, 0, 4, 7, 8, 6],
-        [4, 7, 8, 5, 9, 6, 1, 0, 0],
-        [8, 0, 3, 0, 4, 1, 6, 7, 2],
-        [0, 9, 4, 7, 2, 3, 8, 1, 5],
-        [7, 2, 1, 0, 5, 0, 0, 0, 4],
-      ];
-      expect(() => SudokuSolver.solve(sudoku), throwsException);
-    });
+    group('SudokuSolver exception tests', () {
+      test('Invalid Sudoku - Repetitive numbers', () {
+        var sudoku = <List<int?>>[
+          [2, 1, 7, 4, 3, 5, 9, 2, 8],
+          [5, 8, 9, 0, 6, 2, 4, 3, 0],
+          [3, 4, 6, 0, 7, 9, 2, 5, 1],
+          [1, 6, 2, 3, 0, 0, 5, 4, 0],
+          [9, 0, 5, 2, 0, 4, 7, 8, 6],
+          [4, 7, 8, 5, 9, 6, 1, 0, 0],
+          [8, 0, 3, 0, 4, 1, 6, 7, 2],
+          [0, 9, 4, 7, 2, 3, 8, 1, 5],
+          [7, 2, 1, 0, 5, 0, 0, 0, 4],
+        ];
+        expect(() => SudokuSolver.solve(sudoku), throwsException);
+      });
 
-    test('Invalid Sudoku - Wrong List dimensions (rows)', () {
-      var sudoku = <List<int?>>[
-        [2, 1, 7, 4, 3, 5, 9, 29, 8],
-        [5, 8, 9, 0, 6, 2, 4, 3, 0],
-        [3, 4, 6, 0, 7, 9, 2, 5, 1],
-        [1, 6, 2, 3, 0, 0, 5, 4, 0],
-        [9, 0, 5, 2, 0, 4, 7, 8, 6],
-        [4, 7, 8, 5, 9, 6, 1, 0, 0],
-        [8, 0, 3, 0, 4, 1, 6, 7, 2],
-        [0, 9, 4, 7, 2, 3, 8, 1, 5],
-      ];
-      expect(() => SudokuSolver.solve(sudoku), throwsException);
-    });
+      test('Invalid Sudoku - Wrong numbers', () {
+        var sudoku = <List<int?>>[
+          [2, 1, 7, 4, 3, 5, 9, 29, 8],
+          [5, 8, 9, 0, 6, 2, 4, 3, 0],
+          [3, 4, 6, 0, 7, 9, 2, 5, 1],
+          [1, 6, 2, 3, 0, 0, 5, 4, 0],
+          [9, 0, 5, 2, 0, 4, 7, 8, 6],
+          [4, 7, 8, 5, 9, 6, 1, 0, 0],
+          [8, 0, 3, 0, 4, 1, 6, 7, 2],
+          [0, 9, 4, 7, 2, 3, 8, 1, 5],
+          [7, 2, 1, 0, 5, 0, 0, 0, 4],
+        ];
+        expect(() => SudokuSolver.solve(sudoku), throwsException);
+      });
 
-    test('Invalid Sudoku - Wrong List dimensions (columns)', () {
-      var sudoku = <List<int?>>[
-        [2, 1, 7, 4, 3, 5, 9, 29, 8],
-        [5, 8, 9, 0, 6, 2, 4, 3, 0],
-        [3, 4, 6, 0, 7, 9, 2, 5, 1],
-        [1, 6, 2, 3, 0, 0, 5, 4],
-        [9, 0, 5, 2, 0, 4, 7, 8, 6],
-        [4, 7, 8, 5, 9, 6, 1, 0, 0],
-        [8, 0, 3, 0, 4, 1, 6, 7, 2],
-        [0, 9, 4, 7, 2, 3, 8, 1, 5],
-        [7, 2, 1, 0, 5, 0, 0, 0, 4],
-      ];
-      expect(() => SudokuSolver.solve(sudoku), throwsException);
+      test('Invalid Sudoku - Wrong List dimensions (rows)', () {
+        var sudoku = <List<int?>>[
+          [2, 1, 7, 4, 3, 5, 9, 29, 8],
+          [5, 8, 9, 0, 6, 2, 4, 3, 0],
+          [3, 4, 6, 0, 7, 9, 2, 5, 1],
+          [1, 6, 2, 3, 0, 0, 5, 4, 0],
+          [9, 0, 5, 2, 0, 4, 7, 8, 6],
+          [4, 7, 8, 5, 9, 6, 1, 0, 0],
+          [8, 0, 3, 0, 4, 1, 6, 7, 2],
+          [0, 9, 4, 7, 2, 3, 8, 1, 5],
+        ];
+        expect(() => SudokuSolver.solve(sudoku), throwsException);
+      });
+
+      test('Invalid Sudoku - Wrong List dimensions (columns)', () {
+        var sudoku = <List<int?>>[
+          [2, 1, 7, 4, 3, 5, 9, 29, 8],
+          [5, 8, 9, 0, 6, 2, 4, 3, 0],
+          [3, 4, 6, 0, 7, 9, 2, 5, 1],
+          [1, 6, 2, 3, 0, 0, 5, 4],
+          [9, 0, 5, 2, 0, 4, 7, 8, 6],
+          [4, 7, 8, 5, 9, 6, 1, 0, 0],
+          [8, 0, 3, 0, 4, 1, 6, 7, 2],
+          [0, 9, 4, 7, 2, 3, 8, 1, 5],
+          [7, 2, 1, 0, 5, 0, 0, 0, 4],
+        ];
+        expect(() => SudokuSolver.solve(sudoku), throwsException);
+      });
     });
   });
 }
